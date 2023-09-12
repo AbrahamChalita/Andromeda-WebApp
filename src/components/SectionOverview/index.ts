70d2b06276bfc801b41b1df769a2503b625a5a72
Eduardo AG <edag4000@gmail.com>
2023-09-12 00:20:53 -0400

    Component for statistics added

---
+++ src/components/SectionOverview/index.ts
@@ -0,0 +1,1 @@
+export { default as SectionOverview } from "./SectionOverview"
---
+++ src/components/SectionOverview/types.ts
@@ -0,0 +1,11 @@
+export interface AnswerChipProps {
+    color: string;
+}
+
+export interface SectionOverviewProps {
+    section: number;
+    attempts: number;
+    score: number;
+    time: number;
+    results: number[];
+}
